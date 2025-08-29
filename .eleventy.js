6e7704a40cf0f8409c363c2d29c3c3f56825cbc8
The Keymaker - TGK Admin <the.keymaker@thegnostickey.com>
2025-08-29 01:19:09 +0100

    Fix Eleventy includes: move partials to src/_includes and update base.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,12 @@
 ﻿module.exports = function(eleventyConfig) {
-  // pass-through assets used in pages
   eleventyConfig.addPassthroughCopy({ "src/scripts": "scripts" });
   eleventyConfig.addPassthroughCopy({ "src/css": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/TGK-assets": "TGK-assets" });
 
-  // If/when you move your TGK CSS out of _site, enable this and create the folder:
-  // eleventyConfig.addPassthroughCopy({ "src/css/TGK-main-styles": "css/TGK-main-styles" });
-
   return {
     dir: {
       input: "src",
-      includes: "partials",
+      includes: "_includes",   // <-- default, matches the folder we just used
       layouts: "layouts",
       output: "_site"
     }
